8a8629747f16e0c6c719c0ee634cc1ebbbdc0ea8
Katsumi-N <knaya@MyComputer>
2024-04-06 11:31:35 +0900

    聖遺物ステータスを返せるように修正

--- webapi/enka_response.go
+++ webapi/enka_response.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"strconv"
 )
 
@@ -30,58 +29,11 @@ type EnkaUserInfo struct {
 				Val string `json:"val"`
 			} `json:"4001"`
 		} `json:"propMap"`
-		FightPropMap struct {
-			Num1    float64 `json:"1"`
-			Num2    float64 `json:"2"`
-			Num3    float64 `json:"3"`
-			Num4    float64 `json:"4"`
-			Num5    int     `json:"5"`
-			Num6    float64 `json:"6"`
-			Num7    float64 `json:"7"`
-			Num8    float64 `json:"8"`
-			Num9    float64 `json:"9"`
-			Num20   float64 `json:"20"`
-			Num21   int     `json:"21"`
-			Num22   float64 `json:"22"`
-			Num23   float64 `json:"23"`
-			Num26   int     `json:"26"`
-			Num27   int     `json:"27"`
-			Num28   float64 `json:"28"`
-			Num29   int     `json:"29"`
-			Num30   int     `json:"30"`
-			Num40   int     `json:"40"`
-			Num41   int     `json:"41"`
-			Num42   int     `json:"42"`
-			Num43   float64 `json:"43"`
-			Num44   int     `json:"44"`
-			Num45   int     `json:"45"`
-			Num46   int     `json:"46"`
-			Num50   int     `json:"50"`
-			Num51   int     `json:"51"`
-			Num52   int     `json:"52"`
-			Num53   int     `json:"53"`
-			Num54   int     `json:"54"`
-			Num55   int     `json:"55"`
-			Num56   int     `json:"56"`
-			Num73   int     `json:"73"`
-			Num1003 float64 `json:"1003"`
-			Num1010 float64 `json:"1010"`
-			Num2000 float64 `json:"2000"`
-			Num2001 float64 `json:"2001"`
-			Num2002 float64 `json:"2002"`
-			Num2003 int     `json:"2003"`
-			Num2004 int     `json:"2004"`
-			Num3045 int     `json:"3045"`
-			Num3046 int     `json:"3046"`
-		} `json:"fightPropMap"`
-		SkillDepotID           int   `json:"skillDepotId"`
-		InherentProudSkillList []int `json:"inherentProudSkillList"`
-		SkillLevelMap          struct {
-			Num10731 int `json:"10731"`
-			Num10732 int `json:"10732"`
-			Num10735 int `json:"10735"`
-		} `json:"skillLevelMap"`
-		EquipList []struct {
+		FightPropMap           map[string]float64 `json:"fightPropMap"`
+		SkillDepotID           int                `json:"skillDepotId"`
+		InherentProudSkillList []int              `json:"inherentProudSkillList"`
+		SkillLevelMap          map[string]int     `json:"skillLevelMap"`
+		EquipList              []struct {
 			ItemID    int `json:"itemId"`
 			Reliquary struct {
 				Level            int   `json:"level"`
@@ -112,16 +64,6 @@ type EnkaUserInfo struct {
 					Num114301 int `json:"114301"`
 				} `json:"affixMap"`
 			} `json:"weapon,omitempty"`
-			Flat0 struct {
-				NameTextMapHash string `json:"nameTextMapHash"`
-				RankLevel       int    `json:"rankLevel"`
-				ItemType        string `json:"itemType"`
-				Icon            string `json:"icon"`
-				WeaponStats     []struct {
-					AppendPropID string `json:"appendPropId"`
-					StatValue    int    `json:"statValue"`
-				} `json:"weaponStats"`
-			} `json:"flat,omitempty"`
 		} `json:"equipList"`
 		FetterInfo struct {
 			ExpLevel int `json:"expLevel"`
@@ -146,21 +88,34 @@ const (
 	ElementalMastery
 )
 
-var ArtifactStatTypeMap = map[string]ArtifactStatType{
-	"FIGHT_PROP_HP":                Health,
-	"FIGHT_PROP_HP_PERCENT":        HealthPercent,
-	"FIGHT_PROP_ATTACK":            Attack,
-	"FIGHT_PROP_ATTACK_PERCENT":    AttackPercent,
-	"FIGHT_PROP_DEFENSE":           Defence,
-	"FIGHT_PROP_DEFENSE_PERCENT":   DefencePercent,
-	"FIGHT_PROP_CRITICAL":          CriticalRate,
-	"FIGHT_PROP_CRITICAL_HURT":     CriticalDamage,
-	"FIGHT_PROP_ELEMENT_MASTERY":   ElementalMastery,
-	"FIGHT_PROP_CHARGE_EFFICIENCY": ChargeEfficiency,
+// var ArtifactStatTypeMap = map[string]ArtifactStatType{
+// 	"FIGHT_PROP_HP":                Health,
+// 	"FIGHT_PROP_HP_PERCENT":        HealthPercent,
+// 	"FIGHT_PROP_ATTACK":            Attack,
+// 	"FIGHT_PROP_ATTACK_PERCENT":    AttackPercent,
+// 	"FIGHT_PROP_DEFENSE":           Defence,
+// 	"FIGHT_PROP_DEFENSE_PERCENT":   DefencePercent,
+// 	"FIGHT_PROP_CRITICAL":          CriticalRate,
+// 	"FIGHT_PROP_CRITICAL_HURT":     CriticalDamage,
+// 	"FIGHT_PROP_ELEMENT_MASTERY":   ElementalMastery,
+// 	"FIGHT_PROP_CHARGE_EFFICIENCY": ChargeEfficiency,
+// }
+
+var ArtifactStatTypeMap = map[string]string{
+	"FIGHT_PROP_HP":                "Health",
+	"FIGHT_PROP_HP_PERCENT":        "HealthPercent",
+	"FIGHT_PROP_ATTACK":            "Attack",
+	"FIGHT_PROP_ATTACK_PERCENT":    "AttackPercent",
+	"FIGHT_PROP_DEFENSE":           "Defence",
+	"FIGHT_PROP_DEFENSE_PERCENT":   "DefencePercent",
+	"FIGHT_PROP_CRITICAL":          "CriticalRate",
+	"FIGHT_PROP_CRITICAL_HURT":     "CriticalDamage",
+	"FIGHT_PROP_ELEMENT_MASTERY":   "ElementalMastery",
+	"FIGHT_PROP_CHARGE_EFFICIENCY": "ChargeEfficiency",
 }
 
 type ArtifactStatus struct {
-	Type  ArtifactStatType
+	Type  string
 	Value float64
 }
 
@@ -270,6 +225,6 @@ func (e *EnkaUserInfo) ExtractCharacterStatus() []CharacterStatus {
 			}
 		}
 	}
-	fmt.Printf("%#v\n", characterStatusList)
+
 	return characterStatusList
 }
